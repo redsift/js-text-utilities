127c517fd76b2cf032cc9333b8f48b91b202fd84
Rahul Powar <rahul@redsift.io>
2016-10-24 11:05:37 +0100

    Added test

--- test/split-test.js
+++ test/split-test.js
@@ -24,3 +24,10 @@ tape('split: Junk', function(test) {
   test.equal(words.length, 3);
   test.end();
 });  
+
+tape('split: URL', function(test) {
+  let words = textUtilities.splitWords('Came down with a food bug, hence the erratic hour. Thought of you when reading this : https://www.fastcoexist.com/3064832/in-haiti-a-startup-is-building-100-renewable-grids-for-towns-with-no-power');
+
+  test.equal(words.length, 35);
+  test.end();
+});  
